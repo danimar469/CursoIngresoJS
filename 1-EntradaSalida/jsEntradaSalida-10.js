cc10133c2dbf30cbce9f4a94ce4e0aa6ae11cbbe
danimar469 <david.marzan87@gmail.com>
2019-02-18 15:43:24 -0300

    guardar

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var numero1;
+    var resultado;
+    numero1=parseInt(document.getElementById("importe").value);
+    resultado = numero1*0,25;
+    alert ("el resultado es: " - resultado);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,14 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-    var sueldo;
-    var resultado;
+    var numero1;
+    var numero2;
     var resultado;
 
     numero1 = parseInt(document.getElementById("sueldo").value);
     numero2 = parseInt(document.getElementById("resultado").value);
+    
     resultado = numero1 * 1.1;
-
-    alert ("el resultado es: " + numero2);
+    alert ("el importe con el aumento del 10% es: " + resultado);
 	
 }
